3c99ae86b1fbd5955f8d461ef1ea3ae316786970
l-bianchi <illorenzobianchi@proton.me>
2024-01-31 17:31:07 +0100

    follow quickstart guide

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  fetch() {
+    const body = figlet.textSync("Bun!");
+    return new Response(body);
+  },
+  port: 3000,
+});
